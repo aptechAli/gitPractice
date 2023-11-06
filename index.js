f4f6e170991c9c7002292f61280e66c244844114
Syed Ali Imran Hussain <64258375+SyedALiImran@users.noreply.github.com>
2023-11-06 14:34:08 +0500

    first deploy

---
+++ index.js
@@ -0,0 +1,11 @@
+function registration(){
+    var username = document.getElementById("username").value;
+    var userpass = document.getElementById("userpassword").value;
+    var useremail = document.getElementById("useremail").value;
+
+
+
+    console.log(useremail)
+    console.log(username)
+    console.log(userpass)
+}
